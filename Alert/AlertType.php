7781de6117b6442db9d877588f5728fd925c2e42
Philip Washington Sorst <philip@sorst.net>
2020-01-13 11:09:38 +0100

    Adding flash message support

---
+++ Alert/AlertType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Dontdrinkandroot\BootstrapBundle\Alert;
+
+class AlertType
+{
+    const SUCCESS = 'alert-success';
+    const DANGER = 'alert-danger';
+    const WARNING = 'alert-warning';
+    const INFO = 'alert-info';
+}
