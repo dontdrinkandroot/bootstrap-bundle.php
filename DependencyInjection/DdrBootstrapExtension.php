a9951d505a18b418da6109121d3f8971ec326b1a
Philip Washington Sorst <philip@sorst.net>
2020-12-04 21:48:14 +0100

    Including KnpPaginator configuration/templates

--- DependencyInjection/DdrBootstrapExtension.php
+++ DependencyInjection/DdrBootstrapExtension.php
@@ -4,16 +4,38 @@ namespace Dontdrinkandroot\BootstrapBundle\DependencyInjection;
 
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Knp\Bundle\MenuBundle\KnpMenuBundle;
+use Knp\Bundle\PaginatorBundle\KnpPaginatorBundle;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * @author Philip Washington Sorst <philip@sorst.net>
  */
-class DdrBootstrapExtension extends Extension
+class DdrBootstrapExtension extends Extension implements PrependExtensionInterface
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $bundles = $container->getParameter('kernel.bundles');
+        if (in_array(KnpPaginatorBundle::class, $bundles)) {
+            $container->prependExtensionConfig(
+                'knp_paginator',
+                [
+                    'template' => [
+                        'pagination' => '@KnpPaginator/Pagination/twitter_bootstrap_v4_pagination.html.twig',
+                        'sortable'   => '@DdrBootstrap/KnpPaginator/bs_4_sortable.html.twig',
+                        'filtration' => '@DdrBootstrap/KnpPaginator/bs_4_filtration.html.twig'
+                    ]
+                ]
+            );
+        }
+    }
+
     /**
      * {@inheritdoc}
      */
