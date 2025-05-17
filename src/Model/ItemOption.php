d09cd02f0f50fc9e3f24033a4ea0303f6cf92409
Philip Washington Sorst <philip@sorst.net>
2025-05-17 22:32:56 +0200

    feat: Adding URI ItemOption

--- src/Model/ItemOption.php
+++ src/Model/ItemOption.php
@@ -7,4 +7,5 @@ class ItemOption
     final public const string LABEL = 'label';
     final public const string ROUTE = 'route';
     final public const string ROUTE_PARAMETERS = 'routeParameters';
+    final public const string URI = 'uri';
 }
